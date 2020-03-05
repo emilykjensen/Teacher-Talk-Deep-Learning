1008ca798a5901fff1c7cb6541ef81a62de2c403
Emily Jensen <emje6419@colorado.edu>
2020-03-04 21:53:47 -0700

    start file for generating word embeddings

---
+++ Data/get-word-embeddings.py
@@ -0,0 +1,7 @@
+# reference article
+# https://medium.com/analytics-vidhya/basics-of-using-pre-trained-glove-vectors-in-python-d38905f356db
+
+import numpy as np
+from scipy import spatial
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
