2ff477624c8fb63b4433c90254ff4126f1e31be8
Emily Jensen <emje6419@colorado.edu>
2020-03-04 20:58:45 -0700

    extract relevant features from original data set

---
+++ Data/clean-utterance-data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+# this file is not shared publicly
+import_file = "path/to/all_combined_total_features_renamed_spring_only.csv"
+
+data = pd.read_csv(import_file)
+
+columns_to_keep = [
+        # metadata
+        'ObsID',
+        'UtteranceID',
+        'TeacherID',
+        # text of the utterance
+        'TranscribedUtterance',
+        # labels for talk features
+        'IsQuestion',
+        'IsInstructionalUtterance',
+        'IsInstructionalStatement',
+        'IsDisciplinaryUtterance',
+        'IsDisciplinaryStatement',
+        'IsEvaluationFollowupIncluded',
+        'IsEvaluationElaborated',
+        'IsEvaluationValencePositive',
+        'CombinedAuthCogUptake',
+        'CogLevel',
+        'Uptake',
+        'IsGoalSpecified',
+        'IsDisciplinaryTermsPresent',
+        'IsInstructionalQuestion',
+        'IsDisciplinaryQuestion',
+        'IsStudentResponsePresent',
+        'Authenticity',
+        'IsSerialQuestion',
+        ]
+
+data_export = data[columns_to_keep]
+
+# this file is not shared publicly
+data_export.to_csv('utterance-data-with-labels.csv')
